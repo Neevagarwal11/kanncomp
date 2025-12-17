101b9c8beb645d9bf07ea74472ecaa83fef16cf9
Neev Agarwal <agarwalneev2005@gmail.com>
2025-12-17 20:39:51 +0530

    Deployment Ready

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import About from "./pages/about";
 import { LoaderProvider, useLoader } from "./components/loader";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
 import kci from "./assets/kcilogo.png";
 
 function App() {
@@ -40,35 +39,6 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
 
-        <Helmet>
-          <title>Kanncomp India</title>
-          <meta name="description" content="Kanncomp India" />
-          <link rel="icon" type="image/png" href={kci} />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta property="og:title" content="Kanncomp India" />
-          <meta property="og:image" content="/og-image.png" />
-          <meta property="og:type" content="website" />
-        </Helmet>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import { LoaderProvider } from './components/loader.tsx';
-import { HelmetProvider } from "react-helmet-async";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <LoaderProvider>
-      <HelmetProvider>
       <App />
-      </HelmetProvider>
     </LoaderProvider>
     </BrowserRouter>
   </StrictMode>
